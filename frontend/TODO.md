9a581cd077ea4c1e323b8e8830e967fa1276e420
Brian Lauber <constructible.truth@gmail.com>
2019-09-01 16:22:15 -0400

    Added a TODO.md

---
+++ frontend/TODO.md
@@ -0,0 +1,4 @@
+# TODO #
+
+- Finish configuring [Babel Plugin for Styled Components](https://github.com/styled-components/babel-plugin-styled-components)
+
--- frontend/src/Battle.tsx
+++ frontend/src/Battle.tsx
@@ -3,13 +3,24 @@ import React from 'react';
 
 import styled from 'styled-components';
 
+const Panel = styled.div `
+  border: 1px solid black;
+`;
 
 const Battle: React.FC = ()=> (
-    <div>
-        <button>Attack</button>
-        <button>Item</button>
-        <button>Run</button>
-    </div>
+  <div>
+    <Panel>
+      <button>Attack</button>
+      <button>Item</button>
+      <button>Run!!!</button>
+    </Panel>
+    <Panel>
+      <ul>
+        <li>Thug (3)</li>
+        <li>Hooligan (1)</li>
+      </ul>
+    </Panel>
+  </div>
 );
 
 export default Battle;
