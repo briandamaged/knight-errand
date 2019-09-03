17408b9521126682f17b6035ed45f4a05257878a
Brian Lauber <constructible.truth@gmail.com>
2019-09-02 20:42:31 -0400

    Messed around w/ styled components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,32 +1,11 @@
 import React from 'react';
 import styled from 'styled-components';
 
-import logo from './logo.svg';
-import './App.css';
 
 import Battle from './Battle';
 import Shoppe from './mocks/Shoppe';
 
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-
-const Home: React.FC = ()=> (
-  <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>
-        Edit <code>src/App.tsx</code> and save to reload!!!
-      </p>
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Learn React
-      </a>
-    </header>
-  </div>
-);
 
 
 const Navbar = styled.nav `
@@ -42,14 +21,12 @@ const App: React.FC = ()=> (
   <Router>
     <Navbar>
       <ul>
-        <li><Link to="/">Home</Link></li>
         <li><Link to="/battle">Battle</Link></li>
         <li><Link to="/shoppe" >Shoppe</Link></li>
       </ul>
 
     </Navbar>
     <Viewport>
-      <Route path="/" exact component={Home} />
       <Route path="/battle" exact component={Battle} />
       <Route path="/shoppe" exact component={Shoppe} />
     </Viewport>
--- frontend/src/mocks/Shoppe/index.tsx
+++ frontend/src/mocks/Shoppe/index.tsx
@@ -3,38 +3,98 @@ import React from 'react';
 
 import styled from 'styled-components';
 
-const armors = [
+interface Item {
+  name: string;
+  description: string;
+  price: number;
+}
+
+const armors: Item[] = [
   {
     name: "Pocket Protector",
     description: "Offers perfect protection as long as the enemy hits your pocket.  Otherwise, it's useless.",
+    price: 10.0,
   },
   {
     name: "Cliché Armor for Women",
     description: "Offers no protection whatsoever.",
+    price: 250.0,
   },
   {
     name: "Cliché Armor for Men",
     description: "So heavy that it is impossible to move",
+    price: 250.0,
   },
   {
     name: "Wooden Bucket",
     description: "Decent protection for your head.  Occasionally engulfs your entire head and causes temporary blindness.",
+    price: 25.0,
   },
   {
     name: "Trashcan Lid",
     description: "A makeshift shield.  The smell attracts monsters.",
+    price: 10.0,
   }
 ];
 
+
+const ItemList = styled.ul `
+  border: 1px solid black;
+  background: #222222;
+  padding: 0;
+`;
+
+
+const _ItemEntry = styled.li `
+  list-style-type: none;
+  border: 1px solid black;
+  background: #eeeeee;
+  margin: 0.1em;
+  padding: 0.25em;
+
+  display: grid;
+  grid-template-columns: auto 20%;
+  grid-template-rows: 50% 50%;
+`;
+
+
+const ItemName = styled.h3 `
+  grid-column-start: 1;
+  grid-row-start: 1;
+`;
+
+const ItemDescription = styled.p `
+  grid-column-start: 1;
+  grid-row-start: 2;
+`;
+
+const ItemPrice = styled.p `
+  grid-column-start: 2;
+  grid-row-start: 1;
+`;
+
+const PurchaseButton = styled.button `
+  grid-column-start: 2;
+  grid-row-start: 2;
+`
+
+
+const ItemEntry: React.FC<Item> = (item)=> (
+  <_ItemEntry>
+    <ItemName>{item.name}</ItemName>
+    <ItemDescription>{item.description}</ItemDescription>
+    <ItemPrice>{item.price}</ItemPrice>
+    <PurchaseButton>Purchase</PurchaseButton>
+  </_ItemEntry>
+);
+
+
 const Shoppe: React.FC = ()=> (
-  <dl>
-    { armors.map(({name, description})=> (
-      <>
-        <dt>{name}</dt>
-        <dd>{description}</dd>
-      </>
+  <ItemList>
+    { armors.map((item)=> (
+      <ItemEntry {...item} />
     ))}
-  </dl>
+  </ItemList>
 )
 
 export default Shoppe;
