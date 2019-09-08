982b14dc46a23aa78120ab775d81df1998e7afd8
Brian Lauber <constructible.truth@gmail.com>
2019-09-08 19:50:49 -0400

    WebSockets + Koa

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,4 +1,7 @@
 
+const http = require('http');
+
+const WebSocket = require('ws');
 const Koa = require('koa');
 
 const app = new Koa();
@@ -9,7 +12,35 @@ app.use(function(ctx) {
   };
 });
 
-
-app.listen(3000, function() {
-  console.log("Running on port 3000");
+const wss = new WebSocket.Server({
+  noServer: true,
 });
+
+wss.on('connection', function connection(ws) {
+  ws.on('message', function incoming(message) {
+    console.log('received: %s', message);
+  });
+
+  let i = 0;
+  setInterval(function() {
+    ws.send(`${++i}`);
+  }, 1000);
+
+});
+
+
+const server = http.createServer(app.callback());
+
+server.on('upgrade', function(request, socket, head) {
+  wss.handleUpgrade(request, socket, head, function done(ws) {
+    wss.emit('connection', ws, request);
+  });
+});
+
+
+
+server.listen(3000, function() {
+  console.log("Listening");
+});
+
+
