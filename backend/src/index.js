7dbb81a360f669d7c89463d30236c16a3b193dd5
Brian Lauber <constructible.truth@gmail.com>
2019-09-16 23:17:47 -0400

    Hacked together some inventory commands

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,6 +7,7 @@ const Location = require('./Location');
 
 const {look} = require('./commands/look');
 const {eat} = require('./commands/eat');
+const {inventory} = require('./commands/inventory');
 
 const engine = new GameEngine();
 
@@ -48,6 +49,12 @@ function eatHandler({sender, cmd}) {
 }
 
 
+function inventoryHandler({sender, cmd}) {
+  if(cmd.name === 'inventory') {
+    return ()=> inventory({sender});
+  }
+}
+
 
 /**
  * Converts a raw input into a command
@@ -71,6 +78,11 @@ function decipher(raw) {
         target: rest,
       };
 
+    case 'inventory':
+    case 'items':
+      return {
+        name: 'inventory',
+      };
   }
 }
 
@@ -91,11 +103,17 @@ wss.on('connection', function connection(ws) {
     beEatenBy(sender) {
       sender.blind = true;
       sender.inform("Oops.  It looks like the apple had already expired.  You're blind now.  Congrats.");
+
+      setTimeout(function() {
+        sender.blind = false;
+        sender.inform("Phew -- you can see again!");
+      }, 10000);
     }
   });
 
   player.handlers.push(lookHandler);
   player.handlers.push(eatHandler);
+  player.handlers.push(inventoryHandler);
 
   player.on('informed', function(development) {
     ws.send(development);
