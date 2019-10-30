0eef75873bcf5c8b856bd435cb52d9c8e9bd461f
Brian Lauber <constructible.truth@gmail.com>
2019-10-30 16:36:39 -0400

    History component looks like it's rendered on an ATARI

--- frontend/src/mocks/Commands/History.tsx
+++ frontend/src/mocks/Commands/History.tsx
@@ -10,21 +10,24 @@ export interface Message {
 }
 
 
-
-const Request = styled.pre `
-  border: 1px solid black;
-  padding: 0.5em;
+const BaseMessage = styled.pre `
+  font-size: 18px;
+  padding: 0 0.5em 0 0.5em;
+  margin: 0.3em;
 
   white-space: pre-wrap;
+  color: white;
+`;
+
+const Request = styled(BaseMessage) `
+  &:before {
+    content: '> ';
+  }
 `;
 
 
-const Response = styled.pre `
-  border: 1px solid black;
-  padding: 0.5em;
-  margin-right: 2.5em;
-
-  white-space: pre-wrap;
+const Response = styled(BaseMessage) `
+  margin-left: 5em;
 `;
 
 
@@ -32,6 +35,8 @@ const HistoryContainer = styled.div `
   height: 100%;
   border: 1px solid black;
   padding: 0.5em;
+
+  background-color: #bbbbff;
 `
 
 const ItemFor = (
